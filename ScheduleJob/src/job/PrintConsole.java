714b8e6e90f5c48c5c19680a4d03b4ba00f684c7
Balaji Gurumurthy <31753033+Balajiiguru@users.noreply.github.com>
2020-10-17 22:35:10 -0700

    Update PrintConsole.java

--- ScheduleJob/src/job/PrintConsole.java
+++ ScheduleJob/src/job/PrintConsole.java
@@ -4,7 +4,7 @@ public class PrintConsole {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hurray, the first Jenkins Job");
+		System.out.println("This is my the first Jenkins Job");
 
 	}
 
